3938443d37a97027dd114987b15466c6dd6a0ac2
tunay-tuncer <tunayekin.tuncer@gmail.com>
2025-05-29 14:20:20 +0300

    logo-fix

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from "../assets/logoYellow.png"
 
 const Heading = () => {
     return (
         <div className='heading-container'>
-            <img src="src/assets/logoYellow.png" alt="logo" />
+            <img src={logo} alt="logo" />
             <div className="heading-text-container">
                 <h1>3D Print Price Calculator</h1>
                 <p>CALCULATE THE AVERAGE PRICE OF YOUR 3D PRINT PROJECT</p>
